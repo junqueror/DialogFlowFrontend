79019ee94e9c28d3e9c9d5c0048739f76a0c9d76
Pablo Martin <pablo.martin.ext@bayer.com>
2018-04-13 11:12:35 +0200

    Testing Heroku deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// make express look in the public directory for assets (css/js/img)
+app.use('/', express.static(`${__dirname}/dist`));
+
+// express will serve up index.html if it doesn't recognize the route
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
