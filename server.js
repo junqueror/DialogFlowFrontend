8fdbde7923372766844c2ca663fc0994e6fd4629
junqueror <35776007+junqueror@users.noreply.github.com>
2018-04-13 13:13:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+console.log(`${port}`);
+
 // make express look in the public directory for assets (css/js/img)
 app.use('/', express.static(`${__dirname}/dist`));
+
+console.log(`aquí`);
+console.log(`${__dirname}`);
 
 // express will serve up index.html if it doesn't recognize the route
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './', 'index.html'));
 });
+
+console.log(`más lejos`);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
